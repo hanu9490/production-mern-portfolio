32523ea8b1af5c2c462f7ad197a29cf36df0b3b9
hanu9490 <hanumanthu9490@gmail.com>
2024-03-14 20:33:40 +0530

    First Commit
    mern portfolio app

---
+++ client/build/precache-manifest.e7de6c3252b6ddb936b5835f51160a8f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd79233dde82b7be5db0c3b510c65ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f71d87be0e4a04517a93",
+    "url": "/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "42c8faed9d226e57ca92",
+    "url": "/static/css/main.0f1b5486.chunk.css"
+  },
+  {
+    "revision": "f71d87be0e4a04517a93",
+    "url": "/static/js/2.20f0cd95.chunk.js"
+  },
+  {
+    "revision": "ee803cc7bfbca2f89977",
+    "url": "/static/js/3.6718023e.chunk.js"
+  },
+  {
+    "revision": "42c8faed9d226e57ca92",
+    "url": "/static/js/main.8bb7e8cf.chunk.js"
+  },
+  {
+    "revision": "a27cbbc24711aec815b0",
+    "url": "/static/js/runtime~main.2946d279.js"
+  },
+  {
+    "revision": "7e65f58ddb3d219cf38978f6a7587460",
+    "url": "/static/media/aboutMe.7e65f58d.jpg"
+  },
+  {
+    "revision": "2a840bfb56603af90926c8e0377cc787",
+    "url": "/static/media/hanumanthResume2024.2a840bfb.pdf"
+  },
+  {
+    "revision": "112c483df237981d655f17e0b6d6c57e",
+    "url": "/static/media/profilePic.112c483d.jpg"
+  }
+]);
